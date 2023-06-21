de873a1e23cf5c68e84fc14be5f0ea3168703a6d
Romi1189 <rominabdavila@gmail.com>
2023-06-20 22:20:30 -0300

    base de datos actualizada

--- muni23.sql
+++ muni23.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 31-05-2023 a las 06:13:34
+-- Tiempo de generación: 21-06-2023 a las 03:19:19
 -- Versión del servidor: 10.4.22-MariaDB
 -- Versión de PHP: 7.4.27
 
@@ -20,42 +20,6 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `muni23`
 --
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `archivo`
---
-
-CREATE TABLE `archivo` (
-  `id` int(11) NOT NULL,
-  `mes` varchar(50) NOT NULL,
-  `anio` int(4) NOT NULL,
-  `categoria` varchar(150) DEFAULT NULL,
-  `nombre` int(150) DEFAULT NULL,
-  `fecha` date DEFAULT NULL,
-  `archivo` longblob DEFAULT NULL,
-  `tipo` varchar(150) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `cargo`
---
-
-CREATE TABLE `cargo` (
-  `id` int(11) NOT NULL,
-  `descripcion` varchar(250) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Volcado de datos para la tabla `cargo`
---
-
-INSERT INTO `cargo` (`id`, `descripcion`) VALUES
-(1, 'administrador'),
-(2, 'usuario');
 
 -- --------------------------------------------------------
 
@@ -87,10 +51,12 @@ CREATE TABLE `persona` (
 --
 
 INSERT INTO `persona` (`id_persona`, `nombre1`, `nombre2`, `apellido1`, `apellido2`, `sexo`, `dni`, `cuil`, `direccion`, `contacto`, `correo`, `categoria`, `cargo`, `sector`, `persona_estado`, `estado`) VALUES
-
+(1, 'Romina', 'Belén', 'Davila', '', 'Femenino', '33836224', '27338362248', 'B° Presidente Peon II Mz.:C C.:1 - Las Tapias - Angaco', '2646089217', 'rominabdavila@gmail.com', 8, 'Desarrolador', 1, 'Activo', 0),
+(2, 'Victoria', 'Isabel', 'Davila', '', 'Femenino', '50946765', '27509467655', 'B° Presidente Perón II Mz.:C  C.:1 - Las Tapias - Angaco', '2646089207', '', 2, 'Estudiante', 3, 'Activo', 0),
 (10, 'Cecilia', 'Verónica', 'Davila', '', 'Femenino', '28938940', '27289389402', 'Las Tapias', '2648984909', 'ceciliavdavila@gmail.com', 30, 'Desarrollador', 2, 'Activo', 1),
 (11, 'José', '', 'Perez', '', 'Masculino', '33678987', '', 'Angaco', '2649878038', '', 20, 'Administrativo', 3, 'Activo', 1),
-(12, 'Celeste', 'Rosa', 'Ruiz', 'Cabrera', 'femenino', '28345786', '27283457869', 'B° Bicentenario', '2652354876', 'rositabonita@gmail.com', 16, 'Administrativo', 2, 'activo', 1);
+(12, 'Celeste', 'Rosa', 'Ruiz', 'Cabrera', 'femenino', '28345786', '27283457869', 'B° Bicentenario', '2652354876', 'rositabonita@gmail.com', 16, 'Administrativo', 2, 'activo', 1),
+(13, 'Alejandro', 'Agustin', 'Altamira', '', 'masculino', '23645879', '2023458799', 'Campo de batalla', '2647898123', '', 16, 'administrativo', 0, 'femenino', 1);
 
 -- --------------------------------------------------------
 
@@ -99,62 +65,21 @@ INSERT INTO `persona` (`id_persona`, `nombre1`, `nombre2`, `apellido1`, `apellid
 --
 
 CREATE TABLE `planilla` (
+  `id_img` int(11) NOT NULL,
+  `hoja` int(200) NOT NULL,
   `mes` varchar(15) NOT NULL,
   `anio` int(4) NOT NULL,
-  `numero_exp` int(100) NOT NULL,
-  `estado` int(1) DEFAULT 1,
-  `id_plan` int(100) NOT NULL,
-  `id_persona` int(11) NOT NULL
+  `img` varchar(200) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Volcado de datos para la tabla `planilla`
 --
 
-INSERT INTO `planilla` (`mes`, `anio`, `numero_exp`, `estado`, `id_plan`, `id_persona`) VALUES
-('agosto', 2023, 6798, 0, 1, 1),
-('mayo', 2022, 66677, 1, 2, 3),
-('marzo', 2022, 987, 1, 3, 5),
-('septiembre', 2022, 20324, 1, 4, 2),
-('Abril', 2023, 2354, 1, 5, 4),
-('noviembre', 2022, 678, 1, 6, 6);
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `recibo`
---
-
-CREATE TABLE `recibo` (
-  `id_recibo` int(11) NOT NULL,
-  `anio` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Volcado de datos para la tabla `recibo`
---
-
-INSERT INTO `recibo` (`id_recibo`, `anio`) VALUES
-(1, '2022-03-10'),
-(2, '2022-04-12');
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `t_usuarios`
---
-
-CREATE TABLE `t_usuarios` (
-  `id_usuario` int(11) NOT NULL,
-  `nombre` varchar(30) NOT NULL,
-  `apellido` varchar(30) NOT NULL,
-  `fechanacimiento` date NOT NULL,
-  `correo` varchar(100) NOT NULL,
-  `alias` varchar(30) NOT NULL,
-  `telefono` int(30) NOT NULL,
-  `password` text NOT NULL,
-  `imprimir` datetime DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+INSERT INTO `planilla` (`id_img`, `hoja`, `mes`, `anio`, `img`) VALUES
+(20, 56, 'enero', 2000, 'fotosmuni/LOGO.jpg'),
+(21, 90, 'Abril', 2022, 'fotosmuni/equipo.jpg'),
+(25, 23, 'enero', 2016, 'fotosmuni/ip.PNG');
 
 -- --------------------------------------------------------
 
@@ -163,36 +88,23 @@ CREATE TABLE `t_usuarios` (
 --
 
 CREATE TABLE `usuarios` (
-  `id` int(11) NOT NULL,
-  `nombre` varchar(250) NOT NULL,
-  `usuario` varchar(250) NOT NULL,
-  `contraseña` varchar(250) NOT NULL,
-  `id_cargo` int(11) NOT NULL
+  `id_usu` int(11) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `usuario` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Volcado de datos para la tabla `usuarios`
 --
 
-INSERT INTO `usuarios` (`id`, `nombre`, `usuario`, `contraseña`, `id_cargo`) VALUES
-(1, 'Angelica', 'Angelica', '12345', 1),
-(2, 'Romina', 'Romina', '1234', 2);
+INSERT INTO `usuarios` (`id_usu`, `nombre`, `usuario`, `password`) VALUES
+(1, 'Angelica', 'Angelica', '12345'),
+(2, 'Romina', 'Romi', '1234');
 
 --
 -- Índices para tablas volcadas
 --
-
---
--- Indices de la tabla `archivo`
---
-ALTER TABLE `archivo`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indices de la tabla `cargo`
---
-ALTER TABLE `cargo`
-  ADD PRIMARY KEY (`id`);
 
 --
 -- Indices de la tabla `persona`
@@ -204,76 +116,35 @@ ALTER TABLE `persona`
 -- Indices de la tabla `planilla`
 --
 ALTER TABLE `planilla`
-  ADD PRIMARY KEY (`id_plan`);
-
---
--- Indices de la tabla `recibo`
---
-ALTER TABLE `recibo`
-  ADD PRIMARY KEY (`id_recibo`);
-
---
--- Indices de la tabla `t_usuarios`
---
-ALTER TABLE `t_usuarios`
-  ADD PRIMARY KEY (`id_usuario`);
+  ADD PRIMARY KEY (`id_img`);
 
 --
 -- Indices de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `id_cargo` (`id_cargo`);
+  ADD PRIMARY KEY (`id_usu`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
 --
--- AUTO_INCREMENT de la tabla `archivo`
---
-ALTER TABLE `archivo`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
-
---
--- AUTO_INCREMENT de la tabla `cargo`
---
-ALTER TABLE `cargo`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
 -- AUTO_INCREMENT de la tabla `persona`
 --
 ALTER TABLE `persona`
-  MODIFY `id_persona` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id_persona` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT de la tabla `planilla`
 --
 ALTER TABLE `planilla`
-  MODIFY `id_plan` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
-
---
--- AUTO_INCREMENT de la tabla `t_usuarios`
---
-ALTER TABLE `t_usuarios`
-  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_img` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- Restricciones para tablas volcadas
---
-
---
--- Filtros para la tabla `usuarios`
---
-ALTER TABLE `usuarios`
-  ADD CONSTRAINT `usuarios_ibfk_1` FOREIGN KEY (`id_cargo`) REFERENCES `cargo` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  MODIFY `id_usu` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
